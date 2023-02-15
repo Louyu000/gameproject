05743041b7f4902152b5e29266552a2fad87783f
Louyu000 <louyu50310@gmail.com>
2023-02-15 11:14:03 +0800

    Initial Commit

---
+++ gameproject/gameprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameprojectApp.swift
+//  gameproject
+//
+//  Created by Lou on 2023/2/15.
+//
+
+import SwiftUI
+
+@main
+struct gameprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
